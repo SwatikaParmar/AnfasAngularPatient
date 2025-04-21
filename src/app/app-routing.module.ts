23d167d0dd837cddc77711bcafde4fa41f25c034
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2025-04-21 12:48:02 +0530

    set code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/auths/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'home',
+    path: 'dashboard',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   // {
