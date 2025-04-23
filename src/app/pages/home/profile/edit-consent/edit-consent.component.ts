b5ee2d49bcdc5d9c31d078a6455fd1b3b41d9409
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2025-04-23 12:11:51 +0530

    set code

---
+++ src/app/pages/home/profile/edit-consent/edit-consent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-consent',
+  templateUrl: './edit-consent.component.html',
+  styleUrls: ['./edit-consent.component.css']
+})
+export class EditConsentComponent {
+
+}
