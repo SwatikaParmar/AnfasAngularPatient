e033f8b9b77edf76860eb835cdbe3cdd432021a3
sumitkumar953 <sumitkumar8580589@gmail.com>
2025-04-23 15:52:04 +0530

    code

--- src/app/pages/home/satisfaction/satisfaction-form/satisfaction-form.component.ts
+++ src/app/pages/home/satisfaction/satisfaction-form/satisfaction-form.component.ts
@@ -4,7 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-satisfaction-form',
   templateUrl: './satisfaction-form.component.html',
   styleUrls: ['./satisfaction-form.component.css']
+  
 })
 export class SatisfactionFormComponent {
+  selectedRating = 0;
+}
+export class CardRatingComponent {
+  rating1 = 0;
+  rating2 = 1; // prefilled like in your image
+  rating3 = 0;
+  selectedRating = 0;
 
+  setRating(field: number, value: number) {
+    if (field === 1) this.rating1 = value;
+    else if (field === 2) this.rating2 = value;
+    else if (field === 3) this.rating3 = value;
+  }
+  
+  rate(rating: number) {
+    this.selectedRating = rating;
+    console.log("Rated:", rating);
+  }
 }
