c3fc61bd9fcb4b6c41ebb8b25ce55a77db4a8ea4
aditiranaworld24 <aditiranaworld24@gmail.com>
2025-05-15 16:03:53 +0530

    code

--- src/app/pages/home/satisfaction/satisfaction-form/satisfaction-form.component.ts
+++ src/app/pages/home/satisfaction/satisfaction-form/satisfaction-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-satisfaction-form',
@@ -7,7 +10,56 @@ import { Component } from '@angular/core';
   
 })
 export class SatisfactionFormComponent {
+ Form!: FormGroup;
+  submitted = false;
+  rootUrl: any;
   selectedRating = 0;
+
+  selectedRatings: { [key: string]: number } = {};
+  id: any;
+  constructor(
+    private fb: FormBuilder,
+    private _location: Location,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+    this.rootUrl = environment.rootPathUrl;
+    this.id = this.route.snapshot.paramMap.get('id');
+
+  }
+
+   initForm() {
+    this.Form = this.fb.group(
+      {
+        id: [0],
+        appointmentNumber: [''],
+        mrn: [''],
+        visitId: [''],
+        dateOfVisit: [new Date().toISOString().substring(0, 10)],
+        department: [''],
+        appointmentEase: [''],
+        appointmentWaitTime: [''],
+        frontDeskFriendliness: [''],
+        facilityCleanliness: [''],
+        parkingAvailability: [''],
+        waitingAreaComfort: [''],
+        doctorFriendliness: [''],
+        doctorExplanation: [''],
+        doctorTimeSpent: [''],
+        treatmentQuality: [''],
+        medicationAvailability: [''],
+        treatmentExplanation: [''],
+        overallSatisfaction: [''],
+        recommendationLikelihood: [false],
+        additionalComments: [false]
+      }
+
+    );
+  }
+
+
 }
 export class CardRatingComponent {
   rating1 = 0;
