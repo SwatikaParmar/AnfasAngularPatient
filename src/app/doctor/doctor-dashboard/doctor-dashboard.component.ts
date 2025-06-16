2478bccb95533a9e1f16c8c7763570f3eb0f449e
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2025-06-16 17:30:25 +0530

    set code

--- src/app/doctor/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/doctor/doctor-dashboard/doctor-dashboard.component.ts
@@ -6,6 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctor-dashboard.component.css']
 })
 export class DoctorDashboardComponent {
+images: string[] = [
+    'assets/images/Photo3.jpg',
+    'assets/images/Photo2.jpg',
 
+  ];
+    currentIndex: number = 0;
   fname = localStorage.getItem('dname')
+
+   prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.updateCarousel();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.updateCarousel();
+  }
+
+    updateCarousel() {
+    // Update image opacity (ngClass does this now)
+    // Update dots (ngClass does this now)
+  }
+
+   goToSlide(index: number) {
+    this.currentIndex = index;
+    this.updateCarousel();
+  }
+  
 }
