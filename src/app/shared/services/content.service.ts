b22eb2bc7a949327110e26d747bf5129b8023c19
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2025-06-19 16:40:33 +0530

    set code

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -234,6 +234,8 @@ return this.http.post<any>(environment.apiUrl + ApiEndPoint.patientAssign,data)
     );
   }
 
+  
+
   geteducationalMaterialDetail(id: any): Observable<any> {
     return this.http.get<any>(
       `${environment.apiUrl}${ApiEndPoint.educationalMaterialDetail}?id=${id}`
@@ -306,6 +308,12 @@ return this.http.post<any>(environment.apiUrl + ApiEndPoint.patientAssign,data)
     );
   }
 
+
+  getPatientReport(data:any){
+    return this.http.get<any>(environment.apiUrl +  ApiEndPoint.patientReport + '?PatientUid=' + data.PatientUid 
+      + '&PatientVisitUid=' + data.PatientVisitUid + '&ReportType=' + data.ReportType
+    )
+  }
 
   getDoctorVisit(data:any){
         return this.http.get<any>(environment.apiUrl + ApiEndPoint.visitList + '?mrn=' + data + '&forPatient=false' + '&forDoctor=true')
